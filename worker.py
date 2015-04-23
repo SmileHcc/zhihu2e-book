63ad81142e20329d1990abb9c802fe8bac55561b
SmileHcc <2471773346@qq.com>
2015-04-23 21:59:40 +0800

    improvement worker.py but not complete
    improvement worker.py but not complete

--- worker.py
+++ worker.py
@@ -197,3 +197,201 @@ class QuestionWorker(PageWorker):
         self.maxTry = 5
         self.waitFor = 5
         return
+
+class AuthorWorker(PageWorker):
+    def start(self):
+        self.complete = set()
+        maxTry = self.maxTry
+        while maxTry > 0 and len(self.workSchedule) > 0:
+            self.leader()
+            maxTry -= 1
+        return
+
+    def getIndexID(self):
+        u'''
+        用于为Topic，collection，userAgree添加Index
+        '''
+        return
+
+    def clearIndex(self):
+        u'''
+        用于在插入Index之前先清空Index表
+        '''
+        return
+
+    def addIndex(self, answerHref):
+        u'''
+        用于插入Index
+        '''
+        return
+
+    def leader(self):
+        #clear index cache
+        self.getIndexID()
+        self.clearIndex()
+
+        self.catchFrontInfo()
+        threadPool = []
+        self.questionInfoDictList = []
+        self.answerDictList       = []
+        for key in self.workSchedule:
+            threadPool.append(threading.Thread(target = self.worker, kwargs = {'workNo' : key}))
+        threadsCount = len(threadPool)
+        threadLiving = 2
+        while (threadsCount > 0 or threadLiving > 1):
+            bufLength = self.maxThread - threadLiving
+            if bufLength > 0 and threadsCount > 0:
+                while bufLength > 0 and threadsCount > 0:
+                    threadPool[threadsCount - 1].start()
+                    bufLength -= 1
+                    threadsCount -= 1
+                    time.sleep(0.1)
+            else:
+                print u'正在读取答案页面，还有{}/{}张页面等待读取'.format(len(self.workSchedule) - len(self.complete), len(self.workSchedule))
+                time.sleep(1)
+            threadLiving = threading.activeCount()
+        for questionInfoDict in self.questionInfoDictList:
+            self.save2DB(self.cursor, questionInfoDict, 'questionIDinQuestionDesc', 'QuestionInfo')
+        for answerDict in self.answerDictList:
+            self.save2DB(self.cursor, answerDict, 'answerHref', 'AnswerContent')
+            self.addIndex(answerDict['answerHref'])
+        self.conn.commit()
+        print 'commit complete'
+        return
+
+    def catchFrontInfo(self):
+        content = self.getHttpContent(url = self.urlInfo['infoUrl'], extraHeader = self.extraHeader, timeout = self.waitFor)
+        if content == '':
+            return
+        parse    = AuthorInfoParse(content)
+        infoDict = parse.getInfoDict()
+        self.save2DB(self.cursor, infoDict, 'authorID', 'AuthorInfo')
+        return
+
+
+    def worker(self, workNo = 0):
+        u"""
+        worker只执行一次，待全部worker执行完毕后由调用函数决定哪些worker需要再次运行
+        重复的次数由self.maxTry指定
+        """
+        if workNo in self.complete:
+            return
+        content = self.getHttpContent(url = self.workSchedule[workNo], extraHeader = self.extraHeader, timeout = self.waitFor)
+        if content == '':
+            return
+        parse = ParseAuthor(content)
+        questionInfoDictList, answerDictList = parse.getInfoDict()
+        for questionInfoDict in questionInfoDictList:
+            self.questionInfoDictList.append(questionInfoDict)
+        for answerDict in answerDictList:
+            self.answerDictList.append(answerDict)
+        self.complete.add(workNo)
+        return
+
+    def addProperty(self):
+        self.maxPage = 1
+        self.suffix  = '/answers?order_by=vote_num&page='
+        self.maxTry  = 5
+        self.waitFor = 5
+        return
+
+class TopicWorker(AuthorWorker):
+    def getIndexID(self):
+        topicMatch = re.search(r'(?<=www.zhihu.com/topic/)\d{8}', self.url)
+        if topicMatch == None:
+            print u'抱歉，没能在网址中匹配到话题ID'
+            print u'输入的话题网址为：{}'.format(self.url)
+            print u'程序无法继续运行，请检查网址是否正确后重试'
+            exit()
+        self.topicID = topicMatch.group(0)
+        return self.topicID
+
+    def clearIndex(self):
+        self.getIndexID()
+        self.cursor.execute('delete from TopicIndex where topicID = ?', [self.topicID,])
+        self.conn.commit()
+        return
+
+    def addIndex(self, answerHref):
+        self.cursor.execute('replace into TopicIndex (answerHref, topicID) values (?, ?)', [answerHref, self.topicID])
+        return
+
+    def catchFrontInfo(self):
+        content = self.getHttpContent(url = self.urlInfo['infoUrl'], extraHeader = self.extraHeader, timeout = self.waitFor)
+        if content == '':
+            return
+        parse    = TopicInfoParse(content)
+        infoDict = parse.getInfoDict()
+        self.save2DB(self.cursor, infoDict, 'topicID', 'TopicInfo')
+        return
+
+    def worker(self, workNo = 0):
+        if workNo in self.complete:
+            return
+        content = self.getHttpContent(url = self.workSchedule[workNo], extraHeader = self.extraHeader, timeout = self.waitFor)
+        if content == '':
+            return
+        parse = ParseTopic(content)
+        questionInfoDictList, answerDictList = parse.getInfoDict()
+        for questionInfoDict in questionInfoDictList:
+            self.questionInfoDictList.append(questionInfoDict)
+        for answerDict in answerDictList:
+            self.answerDictList.append(answerDict)
+        self.complete.add(workNo)
+        return
+
+    def addProperty(self):
+        self.maxPage = 1
+        self.suffix  = '/top-answers?page='
+        self.maxTry  = 5
+        self.waitFor = 5
+        return
+
+class CollectionWorker(AuthorWorker):
+    def getIndexID(self):
+        collectionMatch = re.search(r'(?<=www.zhihu.com/collection/)\d{8}', self.url)
+        if collectionMatch == None:
+            print u'抱歉，没能在网址中匹配到收藏夹ID'
+            print u'输入的收藏夹网址为：{}'.format(self.url)
+            print u'程序无法继续运行，请检查网址是否正确后重试'
+            exit()
+        self.collectionID = collectionMatch.group(0)
+        return self.collectionID
+
+    def addIndex(self, answerHref):
+        self.cursor.execute('replace into CollectionIndex (answerHref, collectionID) values (?, ?)', [answerHref, self.collectionID])
+        return
+
+    def catchFrontInfo(self):
+        content = self.getHttpContent(url = self.urlInfo['infoUrl'], extraHeader = self.extraHeader, timeout = self.waitFor)
+        if content == '':
+            return
+        parse    = CollectionInfoParse(content)
+        infoDict = parse.getInfoDict()
+        self.save2DB(self.cursor, infoDict, 'collectionID', 'CollectionInfo')
+        return
+
+    def worker(self, workNo = 0):
+        if workNo in self.complete:
+            return
+        content = self.getHttpContent(url = self.workSchedule[workNo], extraHeader = self.extraHeader, timeout = self.waitFor)
+        if content == '':
+            return
+        parse = ParseCollection(content)
+        questionInfoDictList, answerDictList = parse.getInfoDict()
+        for questionInfoDict in questionInfoDictList:
+            self.questionInfoDictList.append(questionInfoDict)
+        for answerDict in answerDictList:
+            self.answerDictList.append(answerDict)
+        self.complete.add(workNo)
+        return
+
+    def addProperty(self):
+        self.maxPage = 1
+        self.suffix  = '?page='
+        self.maxTry  = 5
+        self.waitFor = 5
+        return
+"""
+add other worker
+"""
